b92387086d9c6a96d791e026f936d44ffa69f84e
valmnt <valentinmont8@gmail.com>
2024-09-06 12:44:46 +0200

    first commit

---
+++ Marker/Features/Presentation/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Marker
+//
+//  Created by Valentin Mont on 06/09/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+    }
+}
+
